32e8116608829165e8397d1b653e2746c029de05
Arigo.Fahreza <arigo@jkt1.ebdesk.com>
2023-08-18 14:47:40 +0700

    :tada: Initial commit

---
+++ confluent_kafka_consumer/src/service/consumer.py
@@ -0,0 +1,26 @@
+from confluent_kafka import Consumer
+from pydantic.v1 import BaseSettings
+
+
+class KafkaConfig(BaseSettings):
+    KAFKA_BOOTSTRAP: str
+    KAFKA_GROUP_ID: str
+    KAFKA_AUTO_OFFSET_RESET: str
+    KAFKA_AUTO_COMMIT: str
+    KAFKA_TOPIC: str
+
+    class Config:
+        env_file = ".env"
+
+
+def producer() -> Consumer:
+    kafka_config = KafkaConfig()
+    consumer = Consumer(
+        {
+            "bootstrap.servers": kafka_config.KAFKA_BOOTSTRAP,
+            "group.id": kafka_config.KAFKA_GROUP_ID,
+            "auto.offset.reset": kafka_config.KAFKA_AUTO_OFFSET_RESET
+        }
+    )
+    consumer.subscribe([kafka_config.KAFKA_TOPIC])
+    return consumer
